a164c8392ecc2cd86a1981bf6c24262d0af3bc1f
Rishav <rishav.jnit@gmail.com>
2018-10-21 17:11:18 +0530

    Binary Search but this is wrong if value is not present

---
+++ 3 Searching Algorithms/binarySearch.js
@@ -0,0 +1,20 @@
+/*This is worng Beacuse value is not present 
+then it comes in infinit loop
+*/
+function binarySeatch(arr,val){
+    var left = 0;
+    var right = arr.length - 1;
+    var mid = Math.floor((left+right)/2);
+    while(arr[mid] != val){
+        if(val < arr[mid])
+        {
+            right = mid -1
+        }
+        else{
+            left = mid +1
+        }
+        mid = Math.floor((left+right)/2);
+    }
+    return mid
+}
+console.log(binarySeatch([2,6,7,8,17,18,22,28,45,88,89,100],88));
