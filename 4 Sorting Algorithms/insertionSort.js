ae63d16332326b618534a0b3fca5ab93a074bd5c
Rishav <rishav.jnit@gmail.com>
2018-10-31 18:55:35 +0530

    Insertion

--- 4 Sorting Algorithms/insertionSort.js
+++ 4 Sorting Algorithms/insertionSort.js
@@ -19,4 +19,16 @@ function insertionSort(arr){
    }
    return arr
 }
+function insertionSort(arr){
+  var currentVal;
+    for(var i = 1; i < arr.length; i++){
+        currentVal = arr[i];
+        for(var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+            arr[j+1] = arr[j]
+        }
+        arr[j+1] = currentVal;
+    }
+    return arr;
+}
+
 console.log(insertionSort([7,4,8,3,9,88]));
