03f3f407d4991354289ba7613e78cf4b82e023b3
Rishav <rishav.jnit@gmail.com>
2018-11-01 22:05:34 +0530

    Radix Sort

--- 4 Sorting Algorithms/radixSort.js
+++ 4 Sorting Algorithms/radixSort.js
@@ -21,3 +21,16 @@ function mostDigits(nums){
 	}
 	return maxDigits;
 }
+
+function radixSort(nums){
+	let maxDigitCount = mostDigits(nums);
+	for(let k = 0; k < maxDigitCount; k++){
+		let digitBuckets = Array.from({length:10},()=>[]);
+		for(let i = 0; i < nums.length; i++){
+			let digit = getDigit(nums[i],k);
+			digitBuckets[digit].push(nums[i]);
+		}
+		nums = [].concat(...digitBuckets);
+	}
+	return nums;
+}
