8acfb194e557ba81a384a04985bf1bc95213ab08
Rishav <rishav.jnit@gmail.com>
2018-11-02 21:42:51 +0530

    class in javascript

---
+++ 5 ES2015 Class Syntax/classSyntaxx.js
@@ -0,0 +1,10 @@
+class Student{
+	constructor(firstName,lastName,year){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.year = year;
+	}
+}
+
+let firstStudent = new Student('Rishav','Srivastava','2018')
+let firstStudent = new Student('Maa')
