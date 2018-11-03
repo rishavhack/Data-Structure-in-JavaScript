eac9e530365c37d1382431afc408f7c465c6f02b
Rishav <rishav.jnit@gmail.com>
2018-11-03 18:26:28 +0530

    Pop Method in Singly link list

---
+++ 6 Singly Link List/pop.js
@@ -0,0 +1,42 @@
+class Node{
+	constructor(val){
+		this.val = val;
+		this.next = null;
+	}
+}
+class SinglyLinkedList{
+	constructor(){
+		this.head = null;
+		this.tail = null;
+		this.length = 0;
+	}
+	push(val){
+		let newNode = new Node(val);
+		if(!this.head){
+			this.head = newNode;
+			this.tail = this.head;
+		}else{
+			this.tail.next = newNode;
+			this.tail = newNode;
+		}
+	}
+	pop(){
+		if(!this.head){
+			return undefined;
+		}
+		var current = this.head;
+		var newTail = current;
+		while(current.next){
+			newTail = current;
+			current = current.next;
+		}
+		this.tail = newTail;
+		this.tail = null;
+		this.length--;
+		if(this.length === 0){
+			this.head = null;
+			this.tail = null;
+		}
+		return current
+	}
+}
