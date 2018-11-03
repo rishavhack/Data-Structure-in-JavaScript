64a989e505131c9be51c6847311d766898d14e5c
Rishav <rishav.jnit@gmail.com>
2018-11-03 18:24:55 +0530

    Basic Syntax

--- 6 Singly Link List/Basic Syntax.js
+++ 6 Singly Link List/Basic Syntax.js
@@ -10,18 +10,6 @@ class SinglyLinkedList{
 		this.tail = null;
 		this.length = 0; 
 	}
-	push(val){
-		var newNode = new Node(val);
-		if(!this.head){
-			this.head = newNode;
-			this.tail = this.head;
-		}else{
-			this.tail.next = newNode;
-			this.tail = newNode;
-		}
-		this.length++;
-		return this
-	}
 }
 
 var list = new SinglyLinkedList()
