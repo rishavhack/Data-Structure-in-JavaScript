b33db240854534b942840c2d89b975a85ae1b294
Rishav <rishav.jnit@gmail.com>
2018-11-04 17:28:35 +0530

    Unshift adding value at head

---
+++ 6 Singly Link List/unshift(Add head).js
@@ -0,0 +1,67 @@
+class Node{
+	construtor(val){
+		this.val = val;
+		this.next = null;
+	}
+}
+class SinglyLinkedList{
+	construtor(){
+		this.head =null;
+		this.tail = null;
+		this.length = 0;
+	}
+	push(val){
+		let newNode = new Node(val);
+		if(!this.head){
+			this.head  = newNode;
+			this.tail = this.head;
+		}else{
+			this.tail.next = newNode;
+			this.tail = newNode;
+		}
+		this.length++;
+		return this;
+	}
+	pop(){
+		if(!this.head){
+			return undefined;
+		}
+		let current = this.head;
+		let newTail = current;
+		while(current.next){
+			newTail = current;
+			current = current.next;
+		}
+		this.tail = newTail;
+		this.tail.next = null;
+		this.length--;
+		if(this.length === 0){
+			this.head = null;
+			this.tail = null;
+		}
+		return current
+	}
+	shift(){
+		if(!this.head){
+			return undefined
+		}
+		let currentHead = this.head;
+		this.head = current.next;
+		this.length--;
+		if (this.length === 0) {
+			this.tail = null;
+		}
+		return currentHead;
+	}
+	unshift(val){
+		var newHead = new Node(val);
+		if(!this.head){
+			this.head = newHead;
+			this.tail = this.head;
+		}
+		newHead.next = this.head;
+		this.head  = newHead;
+		this.length++
+		return this;
+	}
+}
