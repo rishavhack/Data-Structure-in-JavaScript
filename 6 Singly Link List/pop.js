bb3f6ba9a4d490bf134e2c0174ecae54926034e2
Rishav <rishav.jnit@gmail.com>
2018-11-04 17:03:46 +0530

    Pop function

--- 6 Singly Link List/pop.js
+++ 6 Singly Link List/pop.js
@@ -17,26 +17,27 @@ class SinglyLinkedList{
 			this.tail = this.head;
 		}else{
 			this.tail.next = newNode;
-			this.tail = newNode;
+			this.tail = newNode; 
 		}
+		this.length ++
+		return this
 	}
 	pop(){
-		if(!this.head){
-			return undefined;
-		}
-		var current = this.head;
-		var newTail = current;
-		while(current.next){
-			newTail = current;
-			current = current.next;
-		}
-		this.tail = newTail;
-		this.tail = null;
-		this.length--;
-		if(this.length === 0){
-			this.head = null;
-			this.tail = null;
-		}
-		return current
-	}
+        if(!this.head) return undefined;
+        var current = this.head;
+        var newTail = current;
+        while(current.next){
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if(this.length === 0){
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+
+    }
 }
