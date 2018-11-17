c951f68d513309d162952582db07416690517a71
Rishav <rishav.jnit@gmail.com>
2018-11-17 20:10:59 +0530

    Pop New code

--- 7 Doubly linked list/pop.js
+++ 7 Doubly linked list/pop.js
@@ -1,40 +1,43 @@
 class Node{
-	constructor(val){
-		this.val = val;
-		this.next = null;
-		this.prev = null;
-	}
+    constructor(val){
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
 }
-class DoublyLinkedList{
-	constructor(){
-		this.head = null;
-		this.tail = null;
-		this.length = 0;
-	}
-	push(val){
-		let newNode = new Node(val);
-		if(!this.head){
-			this.head = newNode;
-			this.tail = newNode;
-		}else{
-			this.tail.next = newNode;
-			newNode.prev = this.tail;
-			this.tail = newNode;
-		}
-		this.length++
-		return this
-	}
-	pop(){
-		if(!this.head){
-			return undefined;
-		}
-		var current = this.head;
-		var newTail = current;
-		while(current){
-			newTail = current;
-			current = current.next;
-		}
-		this.tail = newTail;
-		this.tail.next = null;
-	}
+
+
+class DoublyLinkedList {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val){
+        var newNode = new Node(val);
+        if(this.length === 0){
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    } 
+    pop(){
+        if(!this.head) return undefined;
+        var poppedNode = this.tail;
+        if(this.length === 1){
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = poppedNode.prev;
+            this.tail.next = null;
+            poppedNode.prev = null;
+        }
+        this.length--;
+        return poppedNode;
+    }
 }
