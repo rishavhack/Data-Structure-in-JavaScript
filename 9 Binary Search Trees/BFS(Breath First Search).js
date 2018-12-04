ce4c018f33545fe049e4ced02bc252ef063e78e5
Rishav <rishav.jnit@gmail.com>
2018-12-04 23:11:16 +0530

    Breath First Search

---
+++ 9 Binary Search Trees/BFS(Breath First Search).js
@@ -0,0 +1,80 @@
+class Node{
+	constructor(value){
+		this.value =value;
+		this.left = null;
+		this.right = null;
+	}
+}
+class BinarySearchTree{
+	constructor(){
+		this.root = null
+	}
+	insert(value){
+		var newNode = new Node(value);
+		if(!this.root){
+			this.root = newNode
+			return this
+		}else{
+			var current = this.root
+			while(true){
+				if(value == current.value){
+					return undefined
+				}
+				if(value < current.value){
+					if(current.left == null){
+						current.left = newNode;
+						return this
+					}
+					current = current.left;
+				}else{
+					if(current.right == null){
+						current.right = newNode
+						return this
+					}
+					current = current.right;
+				}
+			}
+		}
+	}
+	find(value){
+		if(!this.root){
+			return undefined
+		}
+		var current. = this.root;
+		var found = false;
+		while(current && !found){
+			if(value < current.value){
+				current = current.left
+			}else if(value > current.value){
+				current = current.right
+			}else{
+				found = true
+			}
+		}
+		if(!found){
+			return undefined
+		}
+		return current
+	}
+	BFS(){
+		var node = this.root;
+		var data = [];
+		var queue = [];
+
+		while(queue.length){
+			node = queue.shift();
+			data.push(node);//data.push(node.value) to see result
+			if(node.left){
+				queue.push(node.left);
+			}
+			if(node.right){
+				queue.push(node.right);
+			}
+			return data
+		}
+	}
+}
+
+
+
+
