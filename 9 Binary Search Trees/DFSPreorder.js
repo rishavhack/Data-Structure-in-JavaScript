f8fb6a38f2969b664c7ee903b234f98109864efa
Rishav <rishav.jnit@gmail.com>
2018-12-06 22:50:15 +0530

    DFSPreorder

--- 9 Binary Search Trees/DFSPreorder.js
+++ 9 Binary Search Trees/DFSPreorder.js
@@ -71,4 +71,22 @@ class BinarySearchTree{
 		return data
 
 	}
+	DFSPreOrder(){
+		var data = [];
+		function traverse(node){
+			data.push(node.value);
+			if(node.left){
+				traverse(node.left);
+			}
+			if(node.right){
+				traverse(node.right);
+			}
+		}
+		traverse(this.root);
+	}
 }
+
+
+
+
+
