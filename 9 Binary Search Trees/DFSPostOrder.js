06c1667b394afc467792b808ae2c1f69d6ccf598
Rishav <rishav.jnit@gmail.com>
2018-12-22 22:30:44 +0530

    DFSPostOrder

--- 9 Binary Search Trees/DFSPostOrder.js
+++ 9 Binary Search Trees/DFSPostOrder.js
@@ -11,34 +11,38 @@ class BinarySearchTree{
 		this.root = null;
 	}
 	insert(value){
-		var newNode = new Node(value);
+		let newNode new Node(value);
 		if(!this.root){
 			this.root = newNode;
+			return this
 		}else{
-			var current = this.root
+			let current = this.root;
 			while(true){
-				if(value == current.value) {
-					return undefined
+				if(current.value === value){
+					return undefined;
 				}
 				if(value < current.value){
-					if(current.left == null){
-						current.left = newNode
-						return this
+					if(current.left === null){
+						current.left = newNode;
+						return newNode;
 					}
 					current = current.left
 				}else{
-					if(current.right == null){
+					if(current.right === null){
 						current.right = newNode;
-						return this
+						return newNode;
 					}
-					current = current.right
+					current = current.right;
 				}
 			}
 		}
 	}
 	find(value){
-		var current = this.root;
+		if(this.root === null){
+			return null;
+		}
 		var found = false;
+		var current = this.root;
 		while(current && !found){
 			if(value < current.value){
 				current = current.left
@@ -49,28 +53,71 @@ class BinarySearchTree{
 			}
 		}
 		if(!found){
-			return false
+			return undefined
 		}
-		return current
+		return current;
 	}
-	BFS(){
+	contain(value){
+		if(this.root === null){
+			return null;
+		}
+		var found = false;
 		var current = this.root;
-		var data = [];
-		var queue = [];
-		queue.push(current);
-		while(queue.length){
-			current = queue.shift();
-			data.push(current.value);
-			if(current.left){
-				queue.push(current.left)
-			}
-			if(current.right){
-				queue.push(current.right)
+		while(current && !found){
+			if(value < current.value){
+				current = current.left
+			}else if(value > current.right){
+				current = current.right
+			}else{
+				return true;
 			}
 		}
-		return data;
+		if(!found){
+			return false;
+		}
 	}
+	 BFS(){
+	 	var node = this.root;
+	 	var data = [];
+	 	var queue = [];
+	 	queue.push(node);
+	 	while(queue.length){
+	 		node = queue.shift();
+	 		data.push(node.value);
+	 		if(node.left) queue.push(node.left);
+	 		if(node.right) queue.push(node.right);
+	 	}
+	 	return data
+	 }
+	 DFSPreOrder(){
+	 	var data = [];
+	 	function traverse(node){
+	 		data.push(node.value);
+	 		if(node.left) traverse(node.left);
+	 		if(node.right) traverse(node.right);
+	 	}
+	 	traverse(this.root)
+	 	return data
 
-
-
+	 }
+	 DFSPostOrder(){
+	 	var data = [];
+	 	function traverse(node){
+	 		if(node.left) traverse(node.traverse);
+	 		if(node.right) traverse(node.traverse);
+	 		data.push(node.value)
+	 	}
+	 	traverse(this.root)
+	 	return data
+	 }
+	 DFSInOrder(){
+	 	var data = [];
+	 	function traverse(node){
+	 		if(node.left) traverse(node.left);
+	 		data.push(node.value);
+	 		if(node.right) traverse(node.right);
+	 	}
+	 	traverse(this.root)
+	 	return data
+	 }
 }
