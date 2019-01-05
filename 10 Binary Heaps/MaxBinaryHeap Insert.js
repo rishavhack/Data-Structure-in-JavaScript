1d98e32905c0463079b8ccda841ae54b78430041
Rishav <rishav.jnit@gmail.com>
2019-01-05 20:56:37 +0530

    Insert heap

--- 10 Binary Heaps/MaxBinaryHeap Insert.js
+++ 10 Binary Heaps/MaxBinaryHeap Insert.js
@@ -1,21 +1,33 @@
-class MaxBinaryHeap{
-	constructor(){
-		this.value = []
-	}
-	insert(element){
-		this.value.push(element);
-		this.bubbleUp();
-	}
-	bubbleUp(){
-		let idx = this.value.length - 1;
-		const element = this.value[idx];
-		while(idx > 0){
-			let parentIdx = Math.floor((idx - 1)/2);
-			let parent = this.value[parentIdx];
-			if(element <= parent) break;
-			this.value[parentIdx] = element;
-			this.value[idx] = parent;
-			idx = parentIdx;
-		}
-	}
+class MaxBinaryHeap {
+    constructor(){
+        this.values = [];
+    }
+    insert(element){
+        this.values.push(element);
+        this.bubbleUp();
+    }
+    bubbleUp(){
+        let idx = this.values.length - 1;
+        const element = this.values[idx];
+        while(idx > 0){
+            let parentIdx = Math.floor((idx - 1)/2);
+            let parent = this.values[parentIdx];
+            if(element <= parent) break;
+            this.values[parentIdx] = element;
+            this.values[idx] = parent;
+            idx = parentIdx;
+        }
+    }
 }
+
+let heap = new MaxBinaryHeap();
+heap.insert(41);
+heap.insert(39);
+heap.insert(33);
+heap.insert(18);
+heap.insert(27);
+heap.insert(12);
+heap.insert(55);
+
+
+
