216b706b8d5ee0cbdec957f1e44b5f75fb911900
Rishav <rishav.jnit@gmail.com>
2019-01-06 20:40:24 +0530

    Done

--- 10 Binary Heaps/MaxBinaryHeap Insert.js
+++ 10 Binary Heaps/MaxBinaryHeap Insert.js
@@ -21,12 +21,6 @@ class MaxBinaryHeap {
 }
 
 let heap = new MaxBinaryHeap();
-heap.insert(41);
-heap.insert(39);
-heap.insert(33);
-heap.insert(18);
-heap.insert(27);
-heap.insert(12);
 heap.insert(55);
 
 
