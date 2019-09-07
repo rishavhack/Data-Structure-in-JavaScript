f275846115c517d21343ebbb5f6738da46c7b237
Rishav <rishav.jnit@gmail.com>
2019-09-07 11:00:00 +0100

    Insertion Sort

--- 4 Sorting Algorithms/revisedAllSorting.js
+++ 4 Sorting Algorithms/revisedAllSorting.js
@@ -1,4 +1,4 @@
-function bubbleSort(argument) {
+function bubbleSort(arr) {
 	for (var i = arr.length; i > 0; i--) {
 		for(var j = 0; j < i - 1; j++){
 			if(arr[j] > arr[j+1]){
@@ -7,6 +7,17 @@ function bubbleSort(argument) {
 				arr[j+1] = temp
 			}
 		}
+	}
+	return arr
+}
+
+function insertionSort(arr){
+	for(var i = 1; i< arr.length;i++){
+		var currentVal = arr[i]
+		for(var j = i-1; j>=0 && arr[j] > currentVal;j--){
+			arr[j+1] = arr[j]
+		}
+		arr[j+1] = currentVal;
 	}
 	return arr
 }
