03100a5330d3a5133f07d5772792ceee71feefc8
Rishav <rishav.jnit@gmail.com>
2019-09-11 10:00:00 +0100

    Selection Search

--- 3 Searching Algorithms/revisedAllSearching.js
+++ 3 Searching Algorithms/revisedAllSearching.js
@@ -24,3 +24,20 @@ function binarySeatch(arr,val){
     }
     return -1
 }
+
+function selectionSort(arr){
+	for(var i = 0; i < arr.length;i++){
+	    var min = i
+	    for(j = i+1;j<arr.length;j++){
+	        if(arr[min] > arr[j]){
+	            min = j
+	        }
+	    }
+	    if(i != min){
+	        var temp = arr[i];
+	        arr[i] = arr[min];
+	        arr[min] = temp
+	    }
+	    return arr;
+	}
+}
