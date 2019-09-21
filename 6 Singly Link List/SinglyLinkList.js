fcf3be5d7a864beea194a098986efa13f1e7c11a
Rishav <rishav.jnit@gmail.com>
2019-09-21 07:45:00 +0100

    Singly link list getNode

--- 6 Singly Link List/SinglyLinkList.js
+++ 6 Singly Link List/SinglyLinkList.js
@@ -65,5 +65,17 @@ class SinglyLinkedList{
         this.length++
         return this
     }
+    getNode(index){
+        if(index < 0 || index >= this.length){
+            return null;
+        }
+        let counter = 0;
+        let current = this.head;
+        while(counter != index){
+            current = current.next;
+            counter++
+        }
+        return current
+    }
 }
 var list = new SinglyLinkedList()
